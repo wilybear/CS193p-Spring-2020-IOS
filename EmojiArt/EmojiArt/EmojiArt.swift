b8269a65e2fad3e0206fb4dae7f77016c6759ece
wilybear <samerj9712@gmail.com>
2021-01-25 21:33:39 +0900

    feat - Lectrue 7 added

---
+++ EmojiArt/EmojiArt/EmojiArt.swift
@@ -0,0 +1,38 @@
+//
+//  EmojiArt.swift
+//  EmojiArt
+//
+//  Created by 김현식 on 2021/01/25.
+//
+
+import Foundation
+
+struct EmojiArt{
+    var backgroundURL: URL?
+    var emojis = [Emoji]()
+    //we can't make emojis private(set) because we want user to change position(x,y) values
+    
+    struct Emoji: Identifiable{
+        let text: String
+        var x: Int  //offset from center
+        var y: Int  //offset from center
+        var size: Int
+        var id:Int      // UUID() //unique ID
+        
+        //nobody can create an Emoji except Emoji itself
+        //fileprivate make this private to this file
+        fileprivate init(text: String, x: Int, y: Int, size: Int, id:Int) {
+            self.text = text
+            self.x = x
+            self.y = y
+            self.size = size
+            self.id = id
+        }
+    }
+    
+    private var uniqueEmojiId = 0
+    mutating func addEmoji(_ text: String, x: Int, y: Int, size: Int){
+        uniqueEmojiId += 1
+        emojis.append(Emoji(text: text, x: x, y: y, size: size, id: uniqueEmojiId))
+    }
+}
